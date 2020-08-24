4b4fdf29ce6a30caae80c4d65924f16c140d5a7b
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-08-24 12:40:58 -0300

    Feature: GoBarber user and session

---
+++ goBarber/src/app/middlewares/auth.js
@@ -0,0 +1,22 @@
+import { promisify } from 'util';
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const auth = req.headers.authorization;
+
+  if (!auth) return res.status(401).json({ error: 'Token not found' });
+
+  const [, token] = auth.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token is invalid' });
+  }
+
+  return next();
+};
---
+++ goBarber/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '220cae2ca6e5a44361444009262381f4',
+  expiresIn: '7d',
+};
