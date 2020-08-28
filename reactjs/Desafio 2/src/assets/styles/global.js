fe0b3b2be8663ceca89db62c3c72efbe3dd4b481
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-08-27 21:58:57 -0300

    Config: Development environment

---
+++ reactjs/Desafio 2/src/assets/styles/global.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  :root{
+    font-size: 62.5%;
+  }
+  *{
+    margin:0;
+    padding:0;
+    outline:0;
+    box-sizing: border-box;
+  }
+  
+  html,body,#root{
+    height:100vh;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  body{
+    background:#7159c1;
+    --webkit-font-smoothing: antialiased !important;
+  }
+
+
+`;
