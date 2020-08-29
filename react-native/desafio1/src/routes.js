493320d9784677417fdfab77a3cd6d72232ed064
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-08-29 13:18:49 -0300

    Config: Development environment

---
+++ react-native/desafio1/src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#7159c1' },
+          headerTitleAlign: 'center',
+          headerTintColor: '#fff',
+          headerBackTitleVisible: false,
+        }}
+      >
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="User" component={User} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
